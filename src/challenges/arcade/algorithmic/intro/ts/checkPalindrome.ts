146d060cd19ef45b2c27457d4d3e077e923d9b4a
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 13:06:36 +0200

    Added 'Arcade, Intro, 3. checkPalindrome'

---
+++ src/challenges/arcade/algorithmic/intro/ts/checkPalindrome.ts
@@ -0,0 +1,26 @@
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 3. checkPalindrome'.
+ *
+ * Given the string, check if it is a palindrome.
+ *
+ * @param {string} inputString
+ * A non-empty string consisting of lowercase characters.
+ * Guaranteed constraints:
+ * <code>1 ≤ inputString.length ≤ 10<sup>5</sup></code>.
+ *
+ * @returns {boolean}
+ * <code>true</code> if <code>inputString</code> is a palindrome,
+ * <code>false</code> otherwise.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/aeworxet|CodeSignal}
+ * {@link https://github.com/aeworxet|GitHub}
+ */
+
+function checkPalindrome(inputString: string): boolean {
+  return inputString === inputString.split('').reverse().join('');
+}
+
+module.exports = checkPalindrome;
