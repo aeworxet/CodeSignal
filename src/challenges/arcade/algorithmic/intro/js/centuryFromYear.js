c29e69c3cdcf68fa7125974f4529ff692fb7fb4d
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 13:02:49 +0200

    Added 'Arcade, Intro, 2. centuryFromYear'

---
+++ src/challenges/arcade/algorithmic/intro/js/centuryFromYear.js
@@ -0,0 +1,28 @@
+'use strict';
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 2. centuryFromYear'.
+ *
+ * Given a year, return the century it is in. The first century spans from the
+ * year 1 up to and including the year 100, the second - from the year 101 up to
+ * and including the year 200, etc.
+ *
+ * @param {number} year
+ * A positive integer, designating the year.
+ * Guaranteed constraints:
+ * <code>1 ≤ year ≤ 2005</code>.
+ *
+ * @returns {number}
+ * Integer. The number of the century the year is in.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/aeworxet|CodeSignal}
+ * {@link https://github.com/aeworxet|GitHub}
+ */
+
+function centuryFromYear(year) {
+  return Math.ceil(year / 100);
+}
+
+module.exports = centuryFromYear;
