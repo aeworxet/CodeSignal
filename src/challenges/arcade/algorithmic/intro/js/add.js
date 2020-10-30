8d12df37eed224aa73d809f224d2d7ebd23ca193
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 12:55:33 +0200

    Added 'Arcade, Intro, 1. add'

---
+++ src/challenges/arcade/algorithmic/intro/js/add.js
@@ -0,0 +1,31 @@
+'use strict';
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 1. add'.
+ *
+ * Write a function that returns the sum of two numbers.
+ *
+ * @param {number} param1
+ * Integer.
+ * Guaranteed constraints:
+ * <code>-1000 ≤ param1 ≤ 1000</code>.
+ *
+ * @param {number} param2
+ * Integer.
+ * Guaranteed constraints:
+ * <code>-1000 ≤ param2 ≤ 1000</code>.
+ *
+ * @returns {number}
+ * Integer. The sum of the two inputs.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/aeworxet|CodeSignal}
+ * {@link https://github.com/aeworxet|GitHub}
+ */
+
+function add(param1, param2) {
+  return param1 + param2;
+}
+
+module.exports = add;
---
+++ src/challenges/arcade/algorithmic/intro/ts/add.ts
@@ -0,0 +1,30 @@
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 1. add'.
+ *
+ * Write a function that returns the sum of two numbers.
+ *
+ * @param {number} param1
+ * Integer.
+ * Guaranteed constraints:
+ * <code>-1000 ≤ param1 ≤ 1000</code>.
+ *
+ * @param {number} param2
+ * Integer.
+ * Guaranteed constraints:
+ * <code>-1000 ≤ param2 ≤ 1000</code>.
+ *
+ * @returns {number}
+ * Integer. The sum of the two inputs.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/aeworxet|CodeSignal}
+ * {@link https://github.com/aeworxet|GitHub}
+ */
+
+function add(param1: number, param2: number): number {
+  return param1 + param2;
+}
+
+module.exports = add;
