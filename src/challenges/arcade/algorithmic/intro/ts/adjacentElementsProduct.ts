d2658fb45188c892cdd7af470a3c3d5ddeefb9f8
Viacheslav Turovskyi <aeworxet@protonmail.com>
2020-10-30 14:23:13 +0200

    Added 'Arcade, Intro, 4. adjacentElementsProduct'

---
+++ src/challenges/arcade/algorithmic/intro/ts/adjacentElementsProduct.ts
@@ -0,0 +1,31 @@
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 4. adjacentElementsProduct'.
+ *
+ * Given an array of integers, find the pair of adjacent elements that has the
+ * largest product and return that product.
+ *
+ * @param {number[]} inputArray
+ * An array of integers containing at least two elements.
+ * Guaranteed constraints:
+ * <code>2 ≤ inputArray.length ≤ 10</code>,
+ * <code>-1000 ≤ inputArray[i] ≤ 1000</code>.
+ *
+ * @returns {number}
+ * Integer. The largest product of adjacent elements.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/aeworxet|CodeSignal}
+ * {@link https://github.com/aeworxet|GitHub}
+ */
+
+function adjacentElementsProduct(inputArray: number[]): number {
+  let largest = -1000;
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    largest = Math.max(largest, inputArray[i] * inputArray[i + 1]);
+  }
+  return largest;
+}
+
+module.exports = adjacentElementsProduct;
