5187bc5c3099f9d68ef186af48d32d3a46a342c1
Viacheslav Turovskyi <aeworxet@protonmail.com>
2021-07-28 16:52:31 +0300

    Added 'Arcade, Intro, 16. Are Similar?'

---
+++ src/challenges/arcade/algorithmic/intro/js/areSimilar.js
@@ -0,0 +1,47 @@
+'use strict';
+/**
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 16. Are Similar?'.
+ *
+ * Two arrays are called <em>similar</em> if one can be obtained from another
+ * by swapping at most one pair of elements in one of the arrays.
+ *
+ * Given two arrays <code>a</code> and <code>b</code>, check whether they are
+ * <em>similar</em>.
+ *
+ * @param {number[]} a
+ * Array of integers.
+ * Guaranteed constraints:
+ * <code>3 ≤ a.length ≤ 10<sup>5</sup></code>,
+ * <code>1 ≤ a[i] ≤ 1000</code>.
+ *
+ * @param {number[]} b
+ * Array of integers of the same length as <code>a</code>.
+ * Guaranteed constraints:
+ * <code>b.length = a.length</code>,
+ * <code>1 ≤ b[i] ≤ 1000</code>.
+ *
+ * @returns {boolean}
+ * <code>true</code> if <code>a</code> and <code>b</code> are similar,
+ * <code>false</code> otherwise.
+ *
+ * @author Viacheslav Turovskyi
+ *
+ * {@link https://app.codesignal.com/profile/aeworxet|CodeSignal}
+ * {@link https://github.com/aeworxet|GitHub}
+ */
+
+function areSimilar(a, b) {
+  const aFiltered = a.filter((value, index) => value !== b[index]);
+  const bFiltered = b.filter((value, index) => value !== a[index]);
+  if (
+    (aFiltered.length < 2 && aFiltered.length === bFiltered.length) ||
+    (aFiltered.length === 2 &&
+      aFiltered.reverse().every((value, index) => value === bFiltered[index]))
+  ) {
+    return true;
+  }
+  return false;
+}
+
+module.exports = areSimilar;
---
+++ test/challenges/arcade/algorithmic/intro/ts/areSimilar.test.ts
@@ -0,0 +1,76 @@
+/**
+ * Jest test for
+ * Solution to a challenge
+ * 'CodeSignal, Arcade, Intro, 16. Are Similar?'.
+ */
+
+import { describe, expect, it } from '@jest/globals';
+import * as basePath from 'app-root-path';
+const areSimilar = require(__filename
+  .replace(basePath.resolve('test'), basePath.resolve('src'))
+  .replace('.test.ts', '.ts'));
+/* eslint-disable max-len */
+describe('Test 1', () => {
+  it('For a: [1, 2, 3], b: [1, 2, 3], the output should be true', () => {
+    expect(areSimilar([1, 2, 3], [1, 2, 3])).toBe(true);
+  });
+});
+describe('Test 2', () => {
+  it('For a: [1, 2, 3], b: [2, 1, 3], the output should be true', () => {
+    expect(areSimilar([1, 2, 3], [2, 1, 3])).toBe(true);
+  });
+});
+describe('Test 3', () => {
+  it('For a: [1, 2, 2], b: [2, 1, 1], the output should be false', () => {
+    expect(areSimilar([1, 2, 2], [2, 1, 1])).toBe(false);
+  });
+});
+describe('Test 4', () => {
+  it('For a: [1, 1, 4], b: [1, 2, 3], the output should be false', () => {
+    expect(areSimilar([1, 1, 4], [1, 2, 3])).toBe(false);
+  });
+});
+describe('Test 5', () => {
+  it('For a: [1, 2, 3], b: [1, 10, 2], the output should be false', () => {
+    expect(areSimilar([1, 2, 3], [1, 10, 2])).toBe(false);
+  });
+});
+describe('Test 6', () => {
+  it('For a: [2, 3, 1], b: [1, 3, 2], the output should be true', () => {
+    expect(areSimilar([2, 3, 1], [1, 3, 2])).toBe(true);
+  });
+});
+describe('Test 7', () => {
+  it('For a: [2, 3, 9], b: [10, 3, 2], the output should be false', () => {
+    expect(areSimilar([2, 3, 9], [10, 3, 2])).toBe(false);
+  });
+});
+describe('Test 8', () => {
+  it('For a: [4, 6, 3], b: [3, 4, 6], the output should be false', () => {
+    expect(areSimilar([4, 6, 3], [3, 4, 6])).toBe(false);
+  });
+});
+describe('Test 9', () => {
+  it('For \
+a: [832, 998, 148, 570, 533, 561, 894, 147, 455, 279], \
+b: [832, 998, 148, 570, 533, 561, 455, 147, 894, 279], the output should be true', () => {
+    expect(
+      areSimilar(
+        [832, 998, 148, 570, 533, 561, 894, 147, 455, 279],
+        [832, 998, 148, 570, 533, 561, 455, 147, 894, 279]
+      )
+    ).toBe(true);
+  });
+});
+describe('Test 10', () => {
+  it('For \
+a: [832, 998, 148, 570, 533, 561, 894, 147, 455, 279], \
+b: [832, 570, 148, 998, 533, 561, 455, 147, 894, 279], the output should be false', () => {
+    expect(
+      areSimilar(
+        [832, 998, 148, 570, 533, 561, 894, 147, 455, 279],
+        [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]
+      )
+    ).toBe(false);
+  });
+});
